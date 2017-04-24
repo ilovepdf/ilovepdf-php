88b834ae7897ec5a36264b9fd7bf2566a4b0e281
maztch <maztch@gmail.com>
2017-04-24 13:09:15 +0200

    lock task

---
+++ src/LockTask.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ilovepdf;
+/**
+ * Class UnlockTask
+ *
+ * @package Ilovepdf
+ */
+class LockTask extends Task
+{
+
+    /**
+     * @var string
+     */
+    public $password;
+
+    /**
+     * UnlockTask constructor.
+     * @param string $publicKey
+     * @param string $secretKey
+     */
+    function __construct($publicKey, $secretKey)
+    {
+        $this->tool = 'lock';
+        parent::__construct($publicKey, $secretKey);
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+}
