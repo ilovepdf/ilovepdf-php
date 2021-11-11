eca3203f267823f63c088d798549f0a3b3d2676a
Leonardo Collazo <leonardo.collazo@ilovepdf.com>
2021-11-11 10:25:59 +0000

    More changes

---
+++ samples/sign_management.php
@@ -0,0 +1,47 @@
+<?php
+//include the autoloader
+require_once('../vendor/autoload.php');
+//if manual installation has been used comment line that requires the autoload and uncomment this line:
+//require_once('../init.php');
+
+use Ilovepdf\SignatureManagement;
+
+$signatureRequest = new SignatureManagement("publickey", "secretkey");
+$signatureToken = "signaturetoken";
+$receiverToken = "receivertoken";
+
+// Get a list of all created signature requests
+$signatureRequest->getSignaturesList();
+
+// Get the first page, with max number of 50 entries per page (default is 20, max is 100).
+$signatureRequest->getSignaturesList(0, 50);
+
+// Get the current status of the signature:
+$signatureRequest->getSignatureStatus($signatureToken);
+
+// Get information about a specific receiver:
+$signatureRequest->getReceiverInfo($receiverToken);
+
+// Download the audit file on the filesystem
+$signatureRequest->downloadAuditFile($signatureToken, "./", "audit3");
+
+// Download the original files on the filesystem:
+$signatureRequest->downloadOriginalFiles($signatureToken, "./", "original3");
+
+// Download the created signed files on the filesystem:
+$signatureRequest->downloadSignedFiles($signatureToken, "./", "signed4");
+
+// Correct the email address of a receiver in the event that the email was delivered to an invalid email address
+$signatureRequest->fixReceiverEmail($receiverToken, "newemail@email.com");
+
+// Correct the mobile number of a signer in the event that the SMS was delivered to an invalid mobile number
+$signatureRequest->fixSignerPhone($receiverToken, "34666666666");
+
+// This endpoint sends an email reminder to pending receivers. It has a daily limit quota (check the docs to know the daily quota)
+$signatureRequest->sendReminders($receiverToken);
+
+// Increase the number of days to '4' in order to prevent the request from expiring and give receivers extra time to perform remaining actions.
+$signatureRequest->increaseExpirationDays($receiverToken, 4);
+
+// Void a signature that is currently in progress
+$signatureRequest->voidSignature($receiverToken);
---
+++ src/SignatureManagement.php
@@ -0,0 +1,204 @@
+<?php
+
+namespace Ilovepdf;
+
+use stdClass;
+use Ilovepdf\Exceptions\DownloadException;
+
+/**
+ * Class SignatureManagement
+ *
+ * @package Ilovepdf
+ */
+class SignatureManagement extends Ilovepdf
+{
+
+    /**
+     * @param int $page
+     * @param int $per_page
+     * 
+     * @return array
+     */
+    public function getSignaturesList($page = 0, $per_page = 20): array
+    {
+        $data = [
+            "page" => $page,
+            "per-page" => $per_page
+        ];
+        $response = parent::sendRequest("get", "signature/list", $data);
+        $headers = $response->headers;
+        $return = [
+            "signatures" => $response->body,
+            "pagination" => [
+                'totalCount' => $headers && isset($headers['X-Pagination-Total-Count']) ? $headers['X-Pagination-Total-Count'] : '',
+                'page' => $headers && isset($headers['X-Pagination-Current-Page']) ? $headers['X-Pagination-Current-Page'] - 1 : '',
+                'pageSize' => $headers && isset($headers['X-Pagination-Per-Page']) ? $headers['X-Pagination-Per-Page'] : ''
+            ]
+        ];
+        return $return;
+    }
+
+    /**
+     * @param string $signatureId
+     * 
+     * @return stdClass
+     */
+    public function getSignatureStatus(string $signatureId): stdClass
+    {
+        return parent::sendRequest("get", "signature/requesterview/{$signatureId}")->body;
+    }
+
+    /**
+     * @param string $signatureId
+     * @param string $pathToSave
+     * @param string $filename
+     * 
+     * @return bool
+     */
+    public function downloadAuditFile(string $signatureId, string $pathToSave, string $filename): bool
+    {
+        $response = parent::sendRequest("get", "signature/{$signatureId}/download-audit");
+        return $this->downloadResponseToFile($response, $pathToSave, $filename);
+    }
+
+    /**
+     * @param string $signatureId
+     * @param string $pathToSave
+     * @param string $filename
+     * 
+     * @return string
+     */
+    public function downloadOriginalFiles(string $signatureId, string $pathToSave, string $filename): string
+    {
+        $response = parent::sendRequest("get", "signature/{$signatureId}/download-original");
+        return $this->downloadResponseToFile($response, $pathToSave, $filename);
+    }
+
+    /**
+     * @param string $signatureId
+     * @param string $pathToSave
+     * @param string $filename
+     * 
+     * @return string
+     */
+    public function downloadSignedFiles(string $signatureId, string $pathToSave, string $filename): string
+    {
+        $response = parent::sendRequest("get", "signature/{$signatureId}/download-signed");
+        return $this->downloadResponseToFile($response, $pathToSave, $filename);
+    }
+
+    /**
+     * @param string $receiverTokenRequester
+     * @param string $newEmail
+     * 
+     * @return bool
+     */
+    public function fixReceiverEmail(string $receiverTokenRequester, string $newEmail): bool
+    {
+        $body = [
+            "email" => $newEmail
+        ];
+        $response = parent::sendRequest("put", "signature/signer/fix-email/{$receiverTokenRequester}", $body, false, true);
+        //if above fails, it throws an exception
+        return true;
+    }
+
+    /**
+     * @param string $signerTokenRequester
+     * @param string $newPhone
+     * 
+     * @return bool
+     */
+    public function fixSignerPhone(string $signerTokenRequester, string $newPhone): bool
+    {
+        $body = [
+            "phone" => $newPhone
+        ];
+        $response = parent::sendRequest("put", "signature/signer/fix-phone/{$signerTokenRequester}", $body, false, true);
+        //if above fails, it throws an exception
+        return true;
+    }
+
+    /**
+     * @param string $receiverTokenRequester
+     * 
+     * @return bool
+     */
+    public function sendReminders(string $receiverTokenRequester): bool
+    {
+        $body = [];
+        $response = parent::sendRequest("post", "signature/sendReminder/{$receiverTokenRequester}", $body, false, true);
+        //if above fails, it throws an exception
+        return true;
+    }
+
+    /**
+     * @param string $receiverTokenRequester
+     * 
+     * @return bool
+     */
+    public function voidSignature(string $receiverTokenRequester): bool
+    {
+        $body = [];
+        $response = parent::sendRequest("put", "signature/void/{$receiverTokenRequester}", $body, false, true);
+        //if above fails, it throws an exception
+        return true;
+    }
+
+    /**
+     * @param string $receiverTokenRequester
+     * @param int $amountOfDays
+     * 
+     * @return bool
+     */
+    public function increaseExpirationDays(string $receiverTokenRequester, int $amountOfDays): bool
+    {
+        $body = [
+            "days" => $amountOfDays
+        ];
+        $response = parent::sendRequest("put", "signature/void/{$receiverTokenRequester}", $body, false, true);
+        //if above fails, it throws an exception
+        return true;
+    }
+
+    /**
+     * @param string $receiverTokenRequester
+     * 
+     * @return stdClass
+     */
+    public function getReceiverInfo(string $receiverTokenRequester): stdClass
+    {
+        return parent::sendRequest("get", "signature/receiver/info/{$receiverTokenRequester}")->body;
+    }
+
+    /**
+     * @param string $tool Api tool to use
+     * @param bool $makeStart Set to false for chained tasks, because we don't need the start
+     *
+     * @return mixed Return implemented Task class for specified tool
+     *
+     * @throws \Exception
+     */
+    public function newTask($tool = '', $makeStart = true)
+    {
+        throw new \Exception("This class is not meant to create a new task; but to manage an existing SignTask");
+    }
+
+    private function getExtensionFromMime(string $mime_type)
+    {
+        return explode("/", $mime_type)[1];
+    }
+
+    protected function downloadResponseToFile($response, string $pathToSave, string $filename): string
+    {
+        if (!is_dir($pathToSave)) {
+            throw new \Exception("{$pathToSave} is not a directory");
+        }
+        $mime_type = $response->headers["Content-Type"];
+        $filePath = "{$pathToSave}/{$filename}." . $this->getExtensionFromMime($mime_type);
+        if (!file_put_contents($filePath, $response->raw_body)) {
+            throw new DownloadException("Download success, but could not save the contents of the file to {$filePath}. Check permissions of the directory", 1, null, $response);
+        }
+        return $filePath;
+    }
+}
